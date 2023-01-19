327d6e9493226f007c4f678f73a089006c963c90
walker <walker6574@icloud.com>
2023-01-19 16:17:07 +0900

    ユニットの選択(ボタン)・ユニットの配置(固定位置)・大砲とバリスタの弾の消滅時間調整

---
+++ Assets/Scripts/Ayumu/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //ÉvÉåÉCÉÑÅ[êî
+    const int PLAYER_MAX = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
