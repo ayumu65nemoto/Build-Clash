c68560754b00153a8107055efccf713854cba09b
walker <walker6574@icloud.com>
2023-01-20 11:51:15 +0900

    ユニットをフリックで選び三か所に配置するスクリプト作成・スワイプ中にその方向に伸びる矢印を実装

---
+++ Assets/Scripts/Ayumu/UnitPositionB.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPositionB : MonoBehaviour
+{
+    public enum FlickDirection
+    {
+        Left,
+        Right,
+        Up,
+    }
+
+    Vector3 _clickStartPosition = Vector2.zero;
+    //閾値
+    [SerializeField] float _threshold = 30;
+    private GameObject _gameManager;
+    private SelectUnit _selectUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameManager = GameObject.FindWithTag("GameManager");
+        _selectUnit = _gameManager.GetComponent<SelectUnit>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //タップした点を取得
+            _clickStartPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            //離した点を取得して、タップした点との距離を測る
+            Vector3 dif = Input.mousePosition - _clickStartPosition;
+
+            //負の値を取る子ともあるのでAbsで絶対値に
+            float abs_x = Mathf.Abs(dif.x);
+            float abs_y = Mathf.Abs(dif.y);
+
+            //閾値(反応する最小値)
+            if (abs_x >= _threshold || abs_y >= _threshold)
+            {
+                if (_selectUnit.push_b == true)
+                {
+                    // 横方向
+                    if (abs_x > abs_y)
+                    {
+                        //Flick(dif.x > 0 ? FlickDirection.Right : FlickDirection.Left);
+                        if (dif.x > 0)
+                        {
+                            Flick(FlickDirection.Right);
+                        }
+                        else
+                        {
+                            Flick(FlickDirection.Left);
+                        }
+                    }
+                    // 縦方向
+                    else
+                    {
+                        Flick(FlickDirection.Up);
+                    }
+                }
+            }
+        }
+    }
+
+    private void Flick(FlickDirection dir)
+    {
+        if (dir == FlickDirection.Left)
+        {
+            _selectUnit.SetUnit(-2, 1, -2);
+        }
+        else if (dir == FlickDirection.Right)
+        {
+            _selectUnit.SetUnit(2, 1, -2);
+        }
+        else if (dir == FlickDirection.Up)
+        {
+            _selectUnit.SetUnit(0, 1, -5);
+        }
+    }
+}
