48bcff8c36fabaf8e4c4981a11330545f940f3d0
MSI\kyaaaaaaaaaaaaaaaaaa <aaa@bbb.ccc>
2023-01-23 15:28:52 +0900

    no message

--- Assets/Scripts/Ayumu/Weapons/BallShot.cs
+++ Assets/Scripts/Ayumu/Weapons/BallShot.cs
@@ -6,38 +6,43 @@ public class BallShot : MonoBehaviour
 {
     public GameObject prefab;
     private int _count;
-    private GameObject _gameObject;
-    private GameManager _gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        _gameObject = GameObject.FindWithTag("GameManager");
-        _gameManager = _gameObject.GetComponent<GameManager>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    //ƒvƒŒƒnƒu‰»‚µ‚½‹…‚ğƒZƒbƒg
+        //    GameObject ball = GameObject.Instantiate(prefab) as GameObject;
+        //    //ƒ}ƒEƒXƒNƒŠƒbƒN‚µ‚½’n“_‚É‹…‚ğ”ò‚Î‚·
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    Vector3 dir = ray.direction;
+        //    //‹…‚É—Í‚ğ‰Á‚¦‚é
+        //    ball.GetComponent<Rigidbody>().AddForce(dir * 2000);
+        //}
+
         _count += 1;
 
-        if (_gameManager.battle == true)
+        // ‚U‚OƒtƒŒ[ƒ€‚²‚Æ‚É–C’e‚ğ”­Ë‚·‚é
+        if (_count % 420 == 0)
         {
-            // ‚U‚OƒtƒŒ[ƒ€‚²‚Æ‚É–C’e‚ğ”­Ë‚·‚é
-            if (_count % 420 == 0)
-            {
-                GameObject ball = Instantiate(prefab, transform.position, Quaternion.identity);
-                Rigidbody ballRb = ball.GetComponent<Rigidbody>();
+            GameObject ball = Instantiate(prefab, transform.position, Quaternion.identity);
+            Rigidbody ballRb = ball.GetComponent<Rigidbody>();
 
-                // ’e‘¬‚Í©—R‚Éİ’è
-                ballRb.AddForce(transform.forward * 1000);
+            // ’e‘¬‚Í©—R‚Éİ’è
+            ballRb.AddForce(transform.forward * 1000);
 
-                //// ”­Ë‰¹‚ğo‚·
-                //AudioSource.PlayClipAtPoint(sound, transform.position);
+            //// ”­Ë‰¹‚ğo‚·
+            //AudioSource.PlayClipAtPoint(sound, transform.position);
 
-                // ‚T•bŒã‚É–C’e‚ğ”j‰ó‚·‚é
-                Destroy(ball, 2.0f);
-            }
+            // ‚T•bŒã‚É–C’e‚ğ”j‰ó‚·‚é
+            Destroy(ball, 5.0f);
         }
     }
 }
