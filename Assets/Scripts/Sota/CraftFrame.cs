82bfa4ff82f2e1317b06134e14b3a8408ddb6df7
MSI\kyaaaaaaaaaaaaaaaaaa <aaa@bbb.ccc>
2023-01-25 17:53:43 +0900

    no message

--- Assets/Scripts/Sota/CraftFrame.cs
+++ Assets/Scripts/Sota/CraftFrame.cs
@@ -16,28 +16,11 @@ public class CraftFrame : MonoBehaviour
     void OnTriggerStay(Collider other)
     {
         craftOK.craftStart = true;
-        if (other.gameObject.tag == "CraftBlock")
-        {
-            craftOK.Death = other.gameObject;
-            craftOK.Once = other.transform.position;
-            craftOK.ReturnCost = 1;
-        }
-        if (other.gameObject.tag == "CraftBlock2")
-        {
-            craftOK.Death = other.gameObject;
-            craftOK.Once = other.transform.position;
-            craftOK.ReturnCost = 3;
-        }
-        if (other.gameObject.tag == "CraftBlock3")
-        {
-            craftOK.Death = other.gameObject;
-            craftOK.Once = other.transform.position;
-            craftOK.ReturnCost = 5;
-        }
+       
     }
     void OnTriggerExit(Collider other)
     {
-        craftOK.craftStart = false;
+        //craftOK.craftStart = false;
         craftOK.Death = null;
     }
 }
