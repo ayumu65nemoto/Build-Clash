def9b139a5473affe17b6cb2a2fe25f17db30a69
walker <walker6574@icloud.com>
2023-01-26 16:05:51 +0900

    SelectUnit,UnitPositionを相手プレイヤーようにしたものを作成

---
+++ Assets/Scripts/Ayumu/SelectUnit2.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class SelectUnit2 : MonoBehaviour
+{
+    //ユニット格納
+    [SerializeField] public GameObject[] units2;
+    //ユニット番号
+    public int selectUnitNumber2;
+    //ボタンフラグ
+    public bool push_a2 = false;
+    public bool push_b2 = false;
+    public bool push_c2 = false;
+    //矢印格納
+    public GameObject[] indicater_a2;
+    public GameObject[] indicater_b2;
+    public GameObject[] indicater_c2;
+    //矢印本体
+    public GameObject _indicater02;
+    public GameObject _indicater12;
+    public GameObject _indicater22;
+    public GameObject _indicater32;
+    public GameObject _indicater42;
+    public GameObject _indicater52;
+    public GameObject _indicater62;
+    public GameObject _indicater72;
+    public GameObject _indicater82;
+    //キャンバス確認のためにPhotonConnecterを取得
+    private PhotonConnecter _photonConnecter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectUnitNumber2 = 0;
+
+        //PhotonConnecter取得
+        _photonConnecter = GetComponent<PhotonConnecter>();
+    }
+
+    public void PushDownA()
+    {
+        push_a2 = true;
+        selectUnitNumber2 = 0;
+        indicater_a2[0].SetActive(true);
+        indicater_a2[1].SetActive(true);
+        indicater_a2[2].SetActive(true);
+    }
+
+    public void PushDownB()
+    {
+        push_b2 = true;
+        selectUnitNumber2 = 1;
+        indicater_b2[0].SetActive(true);
+        indicater_b2[1].SetActive(true);
+        indicater_b2[2].SetActive(true);
+    }
+
+    public void PushDownC()
+    {
+        push_c2 = true;
+        selectUnitNumber2 = 2;
+        indicater_c2[0].SetActive(true);
+        indicater_c2[1].SetActive(true);
+        indicater_c2[2].SetActive(true);
+    }
+
+    public void PushDownD()
+    {
+        selectUnitNumber2 = 3;
+    }
+
+    public void PushDownE()
+    {
+        selectUnitNumber2 = 4;
+    }
+
+    public void PushDownF()
+    {
+        selectUnitNumber2 = 5;
+    }
+
+    public void PushUpA()
+    {
+        push_a2 = false;
+        indicater_a2[0].SetActive(false);
+        indicater_a2[1].SetActive(false);
+        indicater_a2[2].SetActive(false);
+    }
+
+    public void PushUpB()
+    {
+        push_b2 = false;
+        indicater_b2[0].SetActive(false);
+        indicater_b2[1].SetActive(false);
+        indicater_b2[2].SetActive(false);
+    }
+
+    public void PushUpC()
+    {
+        push_c2 = false;
+        indicater_c2[0].SetActive(false);
+        indicater_c2[1].SetActive(false);
+        indicater_c2[2].SetActive(false);
+    }
+
+    public void PushUpF()
+    {
+
+    }
+
+    void Update()
+    {
+        if (_photonConnecter.canvasFlag == true)
+        {
+            _indicater02 = GameObject.FindWithTag("indicater0");
+            _indicater12 = GameObject.FindWithTag("indicater1");
+            _indicater22 = GameObject.FindWithTag("indicater2");
+            _indicater32 = GameObject.FindWithTag("indicater3");
+            _indicater42 = GameObject.FindWithTag("indicater4");
+            _indicater52 = GameObject.FindWithTag("indicater5");
+            _indicater62 = GameObject.FindWithTag("indicater6");
+            _indicater72 = GameObject.FindWithTag("indicater7");
+            _indicater82 = GameObject.FindWithTag("indicater8");
+
+            indicater_a2 = new GameObject[] { _indicater02, _indicater12, _indicater22 };
+            indicater_b2 = new GameObject[] { _indicater32, _indicater42, _indicater52 };
+            indicater_c2 = new GameObject[] { _indicater62, _indicater72, _indicater82 };
+
+            for (int i = 0; i < indicater_a2.Length; i++)
+            {
+                indicater_a2[i].SetActive(false);
+                indicater_b2[i].SetActive(false);
+                indicater_c2[i].SetActive(false);
+            }
+        }
+    }
+
+    public void LatePushUpA()
+    {
+        Invoke("PushUpA", 0.5f);
+    }
+
+    public void LatePushUpB()
+    {
+        Invoke("PushUpB", 0.5f);
+    }
+
+    public void LatePushUpC()
+    {
+        Invoke("PushUpC", 0.5f);
+    }
+
+    public void SetUnit(float vecX, float vecY, float vecZ)
+    {
+        //selectUnitNumber個目のユニットを配置する
+        var set = PhotonNetwork.Instantiate(units2[selectUnitNumber2].name, new Vector3(vecX, vecY, vecZ), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Ayumu/UnitPositionB2.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPositionB2 : MonoBehaviour
+{
+    public enum FlickDirection
+    {
+        Left,
+        Right,
+        Up,
+    }
+
+    Vector3 _clickStartPosition = Vector2.zero;
+    //閾値
+    [SerializeField] float _threshold = 30;
+    private GameObject _gameManager;
+    private SelectUnit2 _selectUnit2;
+    //ユニット配置完了フラグ
+    public bool setUnitB2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameManager = GameObject.FindWithTag("GameManager");
+        _selectUnit2 = _gameManager.GetComponent<SelectUnit2>();
+        setUnitB2 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //タップした点を取得
+            _clickStartPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            //離した点を取得して、タップした点との距離を測る
+            Vector3 dif = Input.mousePosition - _clickStartPosition;
+
+            //負の値を取る子ともあるのでAbsで絶対値に
+            float abs_x = Mathf.Abs(dif.x);
+            float abs_y = Mathf.Abs(dif.y);
+
+            //閾値(反応する最小値)
+            if (abs_x >= _threshold || abs_y >= _threshold)
+            {
+                if (_selectUnit2.push_b2 == true)
+                {
+                    // 横方向
+                    if (abs_x > abs_y)
+                    {
+                        //Flick(dif.x > 0 ? FlickDirection.Right : FlickDirection.Left);
+                        if (dif.x > 0)
+                        {
+                            Flick(FlickDirection.Right);
+                        }
+                        else
+                        {
+                            Flick(FlickDirection.Left);
+                        }
+                    }
+                    // 縦方向
+                    else
+                    {
+                        Flick(FlickDirection.Up);
+                    }
+                }
+            }
+        }
+    }
+
+    private void Flick(FlickDirection dir)
+    {
+        setUnitB2 = true;
+        if (dir == FlickDirection.Left)
+        {
+            _selectUnit2.SetUnit(-10, 1, 1);
+        }
+        else if (dir == FlickDirection.Right)
+        {
+            _selectUnit2.SetUnit(10, 1, 1);
+        }
+        else if (dir == FlickDirection.Up)
+        {
+            _selectUnit2.SetUnit(0, 1, 2);
+        }
+    }
+}
