d017b91dfcbc3eda98a4a8b07d10228bee31879c
walker <walker6574@icloud.com>
2023-01-27 16:49:22 +0900

    色を同期できるようなスクリプトを追加

---
+++ Assets/Scripts/Ayumu/Synchronization.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Synchronization : MonoBehaviourPunCallbacks, IPunObservable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            //データの送信
+            stream.SendNext(gameObject.GetComponent<Renderer>().material.color.r);
+            stream.SendNext(gameObject.GetComponent<Renderer>().material.color.g);
+            stream.SendNext(gameObject.GetComponent<Renderer>().material.color.b);
+            stream.SendNext(gameObject.GetComponent<Renderer>().material.color.a);
+        }
+        else
+        {
+            //データの受信
+            float r = (float)stream.ReceiveNext();
+            float g = (float)stream.ReceiveNext();
+            float b = (float)stream.ReceiveNext();
+            float a = (float)stream.ReceiveNext();
+            gameObject.GetComponent<Renderer>().material.color = new Vector4(r, g, b, a);
+        }
+    }
+}
