5dd97106124133e5c1a6a279a15c45acacc9bf9d
walker <walker6574@icloud.com>
2023-01-30 11:18:10 +0900

    同期通信の記述をPlayerStatusの中に書いてみる

--- Assets/Scripts/Ayumu/PlayerStates.cs
+++ Assets/Scripts/Ayumu/PlayerStates.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
-public class PlayerStates : MonoBehaviour
+public class PlayerStates : MonoBehaviourPunCallbacks, IPunObservable
 {
     public enum PlayerState
     {
@@ -50,6 +52,27 @@ public class PlayerStates : MonoBehaviour
             //質量を10プラスする
             _rb.mass += 100;
             wetFlag = true;
+        }
+    }
+
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            //データの送信
+            stream.SendNext(gameObject.GetComponent<Renderer>().material.color.r);
+            stream.SendNext(gameObject.GetComponent<Renderer>().material.color.g);
+            stream.SendNext(gameObject.GetComponent<Renderer>().material.color.b);
+            stream.SendNext(gameObject.GetComponent<Renderer>().material.color.a);
+        }
+        else
+        {
+            //データの受信
+            float r = (float)stream.ReceiveNext();
+            float g = (float)stream.ReceiveNext();
+            float b = (float)stream.ReceiveNext();
+            float a = (float)stream.ReceiveNext();
+            gameObject.GetComponent<Renderer>().material.color = new Vector4(r, g, b, a);
         }
     }
 }
