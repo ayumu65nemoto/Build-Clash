93a8ae117408b06c2981d2efacbdacae3f24bebb
fujiiakitaka <akitakafujii@gmail.com>
2023-02-02 16:11:18 +0900

    デッキ画面のホームボタン

--- Assets/Scripts/Fujii/DeckManager.cs
+++ Assets/Scripts/Fujii/DeckManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeckManager : MonoBehaviour
 {
     public static DeckManager instance;
     public GameObject[] DeckArrays = new GameObject[3];
+
+    public void OnClickHomeButton()
+    {
+        SceneManager.LoadScene("Home");
+    }
 
     // Start is called before the first frame update
     void Start()
