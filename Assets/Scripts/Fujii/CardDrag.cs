5938bb3606e7bec15e4f46e2366a78140850599f
fujiiakitaka <akitakafujii@gmail.com>
2023-02-02 10:27:34 +0900

    ホーム画面→デッキ画面　移動追加

--- Assets/Scripts/Fujii/CardDrag.cs
+++ Assets/Scripts/Fujii/CardDrag.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class CardDrag : MonoBehaviour
 {
+
+    //ボックスの中にオブジェクトを吸い込んでいい時はFalse
+    public static bool dragFlag;
+
+    public Vector3 StartPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartPos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnMouseDrag()
+    {
+        //ドラッグ中は吸い込んではだめ
+        dragFlag = true;
+        //以下3行はドラッグした時にオブジェクトを動かすコード
+        Vector3 objPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, objPos.z);
+
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    void OnMouseUp()
+    {
+        //ドラッグ終了、吸い込んでよし
+        dragFlag = false;
+
+        if (CardSlot.inSlot == false)
+        {
+            this.transform.position = StartPos;
+        }
     }
 }
+
--- Assets/Scripts/Fujii/CardSlot.cs
+++ Assets/Scripts/Fujii/CardSlot.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CardSlot : MonoBehaviour
 {
+    public static bool inSlot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inSlot = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        inSlot = false;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        inSlot = true;
+
+        //ドラッグしてない時は
+        if (CardDrag.dragFlag == false)
+        {
+            //吸い込む（位置を合わせる）
+            other.transform.position = this.transform.position;
+            //吸い込み終わったらフラグ解放
+            CardDrag.dragFlag = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        inSlot = false;
     }
 }
